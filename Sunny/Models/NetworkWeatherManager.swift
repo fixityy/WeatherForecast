09f7850a850aba9c595634a69773a6bbf139acfb
fixityy <belov.roman.yur@gmail.com>
2022-05-11 13:06:01 +0300

    Add NetworkWeatherManager logic

---
+++ Sunny/Models/NetworkWeatherManager.swift
@@ -0,0 +1,26 @@
+//
+//  NetworkWeatherManager.swift
+//  Sunny
+//
+//  Created by Roman Belov on 11.05.2022.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkWeatherManager {
+    
+    func fetchCurrentWeather(forCity city: String) {
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)"
+        guard let url = URL(string: urlString) else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, response, error in
+            if let data = data {
+                let dataString = String(data: data, encoding: .utf8)
+                print(dataString!)
+            }
+        }
+        task.resume()
+    }
+    
+}
---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Roman Belov on 11.05.2022.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "aa341c82b92c1e2c3b6a2f1d7ead5a23"
